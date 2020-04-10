ae6b108322465764c241dfc4f0e3c34b6a831e0a
Nubli Naim <nubli.naim@gmail.com>
2020-04-11 02:14:40 +0900

    feat: add selection coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
+import { withRouter } from "react-router";
 
 import styled, { css } from 'styled-components';
 
 import catalog from './data';
 
+function selectionToCode (selection: number[]) {
+  return catalog.map((item, itemIndex) => {
+    const maxValue = item.remakes - 1;
+    const maxInBin = maxValue.toString(2);
+    let actualValue = (selection[itemIndex]).toString(2);
+    while (actualValue.length < maxInBin.length) {
+      actualValue = '0' + actualValue;
+    }
+    return actualValue;
+  }).join('');
+}
+
 function App() {
-  const [selection, setSelection] = useState([] as number[]);
+  const [selection, setSelection] = useState(catalog.map(() => 0) as number[]);
 
   const selectItem = (itemIndex: number, variantIndex: number) => {
     return () => {
@@ -14,8 +27,10 @@ function App() {
       setSelection(newSelection);
     };
   };
+  const code = selectionToCode(selection);
   return (
     <MainDiv>
+      <div>{parseInt(code, 2).toString(36)}</div>
       {catalog.map((item, itemIndex) => {
         const remakes: string[] = [];
         for(let x = 0; x < item.remakes; x++) {
@@ -42,13 +57,14 @@ function App() {
 }
 
 const MainDiv = styled.div`
+  background: #333;
   color: #eee;
   padding: 16px;
 `;
 
 const ItemBox = styled.div`
   display: inline-block;
-  padding: 24px;
+  padding: 8px;
   background: #111;
   margin: 8px;
   border-radius: 8px;
@@ -60,7 +76,7 @@ interface ItemVariantBoxProps {
 const ItemVariantBox = styled.div<ItemVariantBoxProps>`
   display: inline-block;
   padding: 8px;
-  transition: transform 0.2s;
+  transition: transform 0.2s, background 0.15s;
   border-radius: 12px;
   cursor: pointer;
 
