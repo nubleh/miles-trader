208813cea80c71315b65f260991e8392ae42aa67
Nubli Naim <nubli.naim@gmail.com>
2020-04-11 04:29:03 +0900

    chore: build into docs

---
+++ docs/precache-manifest.5bdb5b4040b415d05e297278e14359d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6891a1519a74e6fad934c2b25c7670e7",
+    "url": "/miles-trader/index.html"
+  },
+  {
+    "revision": "2f49ce122503df96be3c",
+    "url": "/miles-trader/static/css/main.90c3e137.chunk.css"
+  },
+  {
+    "revision": "910a53b526e9cf4c8663",
+    "url": "/miles-trader/static/js/2.b67f12f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/miles-trader/static/js/2.b67f12f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f49ce122503df96be3c",
+    "url": "/miles-trader/static/js/main.7cd0a7ea.chunk.js"
+  },
+  {
+    "revision": "639a3f1104dc622f5f03",
+    "url": "/miles-trader/static/js/runtime-main.37bb26af.js"
+  }
+]);
